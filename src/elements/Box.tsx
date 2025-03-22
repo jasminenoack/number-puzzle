0d2566963f3a4585ed0acaebd3546c600114f56b
Jasmine Noack <jnoack@understood.org>
2025-03-22 18:53:26 -0400

    add single element

---
+++ src/elements/Box.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Box() {
+    return (
+        <div className="box">
+            15
+        </div>
+    );
+}
+
+export default Box;
---
+++ src/elements/Frame.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from './Box';
+
+function Frame() {
+    return (
+        <div className="frame">
+            <Box />
+        </div>
+    );
+}
+
+export default Frame;
