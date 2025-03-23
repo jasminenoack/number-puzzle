6112ffb1134503584baefbfcf77658625a6e65f6
Jasmine Noack <jnoack@understood.org>
2025-03-22 23:32:18 -0400

    more slight design stuff

--- src/elements/Frame.tsx
+++ src/elements/Frame.tsx
@@ -7,16 +7,20 @@ function Frame({ puzzleState }: { puzzleState: PuzzleState }) {
     const puzzle = puzzleState.state;
     const lastLocation = puzzleState.lastMoveTo;
     return (
-        <div className="frame" style={{ height: puzzle.length * BOX_SIZE, borderWidth: BORDER_WIDTH, width: puzzle[0].length * BOX_SIZE }}>
-            {
-                puzzle.map((row, rowIndex) => (
-                    <div key={rowIndex} className="row" style={{ width: row.length * BOX_SIZE }}>
-                        {row.map((number, colIndex) => (
-                            <Box key={colIndex} value={number} lastLocation={[rowIndex, colIndex] + "" == lastLocation + ""} />
-                        ))}
-                    </div>
-                ))
-            }
+        <div className="frame-wrapper">
+            <div className="frame" style={{ height: puzzle.length * BOX_SIZE, borderWidth: BORDER_WIDTH, width: puzzle[0].length * BOX_SIZE }}>
+                {
+                    puzzle.map((row, rowIndex) => (
+                        <div key={rowIndex} className="row" style={{ width: row.length * BOX_SIZE }}>
+                            {row.map((number, colIndex) => (
+                                <Box key={colIndex} value={number} lastLocation={[rowIndex, colIndex] + "" == lastLocation + ""} />
+                            ))}
+                        </div>
+                    ))
+                }
+
+            </div>
+            Score: {puzzleState.simplePuzzleScore()}
         </div>
     );
 }
